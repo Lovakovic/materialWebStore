aa89d76d80ed59bedd6e4e11691ab61efd6bf3cd
Lovakovic <lovkovdario@gmail.com>
2023-02-26 19:15:03 +0100

    Changed pipe, so it only puts the main address first and touches nothing else.

--- public/app/pipes/address-sort.pipe.ts
+++ public/app/pipes/address-sort.pipe.ts
@@ -4,6 +4,9 @@ import {Address} from "../models/address.model";
 @Pipe({
   name: 'addressSort'
 })
+/**
+ * Puts the main address first, changes nothing else
+ */
 export class AddressSortPipe implements PipeTransform {
 
   transform(addresses: Array<Address> | undefined): Array<Address> | undefined {
@@ -13,24 +16,13 @@ export class AddressSortPipe implements PipeTransform {
 
     const sortedAddresses = [...addresses];
     sortedAddresses.sort((a: Address, b:Address) => {
-      // Main address goes first
       if(a.main && !b.main) {
         return -1;
       } else if (!a.main && b.main) {
         return 1;
       }
 
-      // Then sort by nickname asc
-      if(a.addressNickname && b.addressNickname) {
-        return a.addressNickname.localeCompare(b.addressNickname);
-      } else if(a.addressNickname && !b.addressNickname) {
-        return -1;
-      } else if(!a.addressNickname && b.addressNickname) {
-        return 1;
-      }
-
-      // Then by name asc
-      return a.name.localeCompare(b.name);
+      return 0;
     })
 
     return sortedAddresses;
