68e5f8ce015e52b01fa52f767030b936da52f9ab
Lovakovic <lovkovdario@gmail.com>
2023-02-27 21:55:46 +0100

    Created rough form for adding new address, applied basic validators.

--- public/app/components/new-address/new-address.component.ts
+++ public/app/components/new-address/new-address.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-new-address',
@@ -14,14 +14,28 @@ export class NewAddressComponent {
   newAddressForm = new FormGroup({
     addressNickname: new FormControl(''),
     address: new FormGroup({
-      name: new FormControl(''),
+      name: new FormControl('', [
+          Validators.required
+      ]),
       company: new FormControl(''),
-      street: new FormControl(''),
-      city: new FormControl(''),
+      street: new FormControl('', [
+          Validators.required
+      ]),
+      city: new FormControl('', [
+          Validators.required
+      ]),
       zipCode: new FormControl(''),
-      country: new FormControl(''),
+      country: new FormControl('', [
+          Validators.required
+      ]),
     }),
-    phone: new FormControl(''),
+    phone: new FormControl('', [
+        Validators.required
+    ]),
     deliveryInstructions: new FormControl('')
   });
+
+  onSubmit() {
+    console.log(this.newAddressForm.value);
+  }
 }
