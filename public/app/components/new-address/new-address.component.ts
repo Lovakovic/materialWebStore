aed82fcf8960220d4c6201591157283052cd3f47
Lovakovic <lovkovdario@gmail.com>
2023-02-27 21:22:51 +0100

    Created a rough form for adding an address. Commit before adding a country picker.

--- public/app/components/new-address/new-address.component.ts
+++ public/app/components/new-address/new-address.component.ts
@@ -3,26 +3,25 @@ import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-new-address',
-  templateUrl: './new-address.component.html'
+  templateUrl: './new-address.component.html',
+  styles: [`
+    mat-form-field {
+      width: 100%;
+    }
+  `]
 })
 export class NewAddressComponent {
   newAddressForm = new FormGroup({
-    name: new FormControl('', [
-
-    ]),
-    addressNickname: new FormControl('', [
-
-    ]),
-    phone: new FormControl('', [
-
-    ]),
-    deliveryInstructions: new FormControl(''),
+    addressNickname: new FormControl(''),
     address: new FormGroup({
+      name: new FormControl(''),
       company: new FormControl(''),
       street: new FormControl(''),
       city: new FormControl(''),
       zipCode: new FormControl(''),
       country: new FormControl(''),
-    })
+    }),
+    phone: new FormControl(''),
+    deliveryInstructions: new FormControl('')
   });
 }
