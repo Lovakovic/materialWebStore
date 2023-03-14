5551787451b5fe19a4c6b9c4ee2c7eded16d2b52
Lovakovic <lovakovdario@gmail.com>
2023-03-14 13:46:51 +0100

    Modify `updateCart` procedure, so it can UPDATE, INSERT or DELETE `cartItem`

--- db_DDL.sql
+++ db_DDL.sql
@@ -101,17 +101,25 @@ FROM user JOIN cartItem ON user.id = cartItem.userId
           JOIN product ON cartItem.productId = product.id
 ORDER BY addedAt DESC;
 
+DROP PROCEDURE IF EXISTS updateCart;
 DELIMITER //
 CREATE PROCEDURE updateCart(IN i_userId INT, IN i_productId INT, IN i_quantity INT)
 BEGIN
     DECLARE v_prevQuantity INT DEFAULT 0;
     SELECT quantity INTO v_prevQuantity FROM cartItem WHERE userId = i_userId AND productId = i_productId;
 
-    IF v_prevQuantity != 0 THEN
+    # Item to be removed from cart
+    IF i_quantity = 0 THEN
+        DELETE FROM cartItem WHERE userId = i_userId AND productId = i_productId;
+
+        # Item isn't yet in cart
+    ELSEIF v_prevQuantity = 0 THEN
+        INSERT INTO cartItem (userId, productId, quantity) VALUE (i_userId, i_productId, i_quantity);
+
+        # Item quantity needs to change
+    ELSEIF v_prevQuantity != i_quantity THEN
         UPDATE cartItem SET quantity = i_quantity WHERE userId = i_userId AND productId = i_productId;
-    ELSE
-        INSERT INTO cartItem (userId, productId, quantity) VALUES (i_userId, i_productId, i_quantity);
-    END IF;
+    END IF ;
 END //
 DELIMITER ;
 
