7dd80ef78cde565c9e9acb816b4f73ea19a9bbf5
Lovakovic <lovakovdario@gmail.com>
2023-03-15 13:34:50 +0100

    Switch from manual subscription handling to async pipe

--- public/app/pages/home/components/filters/filters.component.ts
+++ public/app/pages/home/components/filters/filters.component.ts
@@ -1,36 +1,19 @@
-import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {StoreService} from "../../../../services/store.service";
-import {Subscription} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html'
 })
-export class FiltersComponent implements OnInit, OnDestroy {
+export class FiltersComponent {
+  categories$: Observable<string[]> = this.storeService.getCategories();
 
   @Output() showCategory = new EventEmitter<string>();
-  categorySubscription: Subscription | undefined;
-  categories: Array<string> | undefined;
-  selectedCategory: string | undefined;
 
-  constructor(private storeService: StoreService) {
-  }
-
-  ngOnInit(): void {
-    this.categorySubscription = this.storeService.getCategories()
-        .subscribe(res => {
-          this.categories = res;
-        });
-  }
+  constructor(private storeService: StoreService) { }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
-  }
-
-  ngOnDestroy(): void {
-    // Gotta prevent the memory leaks!
-    if(this.categorySubscription) {
-      this.categorySubscription.unsubscribe();
-    }
   }
 }
