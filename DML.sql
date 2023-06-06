65475b3b1785463618ee82b49b3374831f30fa47
Lovakovic <lovkovdario@gmail.com>
2023-06-06 13:27:57 +0200

    Insert admin user

--- DML.sql
+++ DML.sql
@@ -2,14 +2,18 @@
 # Mock data
 #
 INSERT INTO category (name, description) VALUES
-                                             ('Electronics', 'Electronic gadgets such as mobile phones, earphones or bluetooth speakers.'),
-                                             ('Shoes', 'Footwear for men or women.'),
-                                             ('Computer parts', 'Parts for building a computer.');
+ ('Electronics', 'Electronic gadgets such as mobile phones, earphones or bluetooth speakers.'),
+ ('Shoes', 'Footwear for men or women.'),
+ ('Computer parts', 'Parts for building a computer.');
 
 INSERT INTO product (name, price, categoryId, description, image) VALUES
-                                                                      ('Dummy phone', 830, 1, 'Hey, look at me! I am a dummy phone.', 's23_uncropped.png'),
-                                                                      ('Dummy shoe', 130, 2, 'Hey, look at me! I am a dummy sneaker.', 'AF1_low_unc_white.webp'),
-                                                                      ('Dummy part', 1399, 3, 'Hey, look at me! I am a dummy RTX 4090.', '4090_fe.webp');
+  ('Dummy phone', 830, 1, 'Hey, look at me! I am a dummy phone.', 's23_uncropped.png'),
+  ('Dummy shoe', 130, 2, 'Hey, look at me! I am a dummy sneaker.', 'AF1_low_unc_white.webp'),
+  ('Dummy part', 1399, 3, 'Hey, look at me! I am a dummy RTX 4090.', '4090_fe.webp');
+
+# Insert admin with password = adminpass
+INSERT INTO user (id, username, password, email, role) VALUE
+    (1, 'admin', '$2b$10$zr/wOggWaTeD1yzyc65/kezK4e5Mj1.2tZW6IUviAbnaO0AxktBNS', 'admin@mail.com', 'adm');
 
 # A few addresses with different non-mandatory attributes omitted
 INSERT INTO address (userId, name, addressNickname, companyName, street, city, zipCode, country, phone,
@@ -20,6 +24,3 @@ INSERT INTO address (userId, name, street, city, zipCode, country, phone) VALUE
     (1, 'Tony Filipovic', 'Ulica Ive Sanadera 1', 'Rijeka', '51000', 'Croatia', '92 274 1927');
 INSERT INTO address (userId, name, addressNickname, street, city, zipCode, country, phone) VALUE
     (1, 'Ivo Markovic', 'Budapest apartment', 'Harosz Matyak street 4', 'Budapest', '16500', 'Hungary', '92 837 1847');
-
-
-# Insert users through registration page because of password hashing, then alter user to preference through SQL
