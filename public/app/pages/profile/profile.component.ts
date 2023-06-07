15c4ad649de55c50db87d60b4b2c557cb1dae02f
Lovakovic <lovkovdario@gmail.com>
2023-06-07 20:13:59 +0200

    Remove storage of addresses inside of service

--- public/app/pages/profile/profile.component.ts
+++ public/app/pages/profile/profile.component.ts
@@ -12,8 +12,8 @@ export class ProfileComponent implements OnInit {
     constructor(public addressService: AddressService) { }
 
 	ngOnInit(): void {
-		this.addressService.getAddress().subscribe(_addresses =>
-			this.addresses = _addresses);
+		this.addressService.getAddress().subscribe(
+			_addresses => this.addresses = _addresses);
 	}
 
     onAddAddress(addressData: {address: Address, primary: boolean}): void {
